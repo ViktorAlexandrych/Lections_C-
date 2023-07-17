446c04ee9835f11ef0ff7450f089767c28331a64
Alexandrych <vyrupaev.00@gmail.com>
2023-07-17 16:41:34 +1000

    Этот коммит просто сохраняет промежуточный результат

--- Acquaintance3/Program.cs
+++ Acquaintance3/Program.cs
@@ -1,3 +1,6 @@
-﻿double numberA = 856;
-double numberB = 45;
-Console.WriteLine(numberA / numberB);
+﻿double numberA = new Random().Next(1, 10);
+System.Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+System.Console.WriteLine(numberB);
+double result = numberA / numberB;
+Console.WriteLine(result);
--- Functions_array003/Program.cs
+++ Functions_array003/Program.cs
@@ -4,4 +4,4 @@ int n = array.Length;
 int find = 4;
 int index = 0;
 
-while (index < n)
+while (index < n) ;
