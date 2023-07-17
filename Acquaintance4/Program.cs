a69626ae6b1a17451103efafa492c7dd59dcae1a
Alexandrych <vyrupaev.00@gmail.com>
2023-07-17 16:51:32 +1000

    Этот коммит просто сохраняет промежуточный результат

--- Acquaintance4/Program.cs
+++ Acquaintance4/Program.cs
@@ -1,9 +1,11 @@
-﻿
-if (true)
+﻿System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "виктор")
 {
-
+	System.Console.WriteLine("Приветствую вас, Повелитель");
 }
 else
 {
-
+	System.Console.Write("Здаров, салага! Ну и имя же у тебя стремное... ");
+	System.Console.Write(username);
 }
