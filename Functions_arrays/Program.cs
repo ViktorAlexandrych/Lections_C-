9f1829c500ec2e666d0e4857e6eeece2a3352fe7
Alexandrych <vyrupaev.00@gmail.com>
2023-07-18 14:25:45 +1000

    Этот коммит просто сохраняет

--- Functions_arrays/Program.cs
+++ Functions_arrays/Program.cs
@@ -17,6 +17,19 @@ int a3 = 13778;
 int b3 = 23;
 int c3 = 33;
 
+
+//int max = a1;
+//if (b1 > max) max = b1;
+//if (c1 > max) max = c1;
+
+//if (a2 > max) max = a2;
+//if (b2 > max) max = b2;
+//if (c2 > max) max = c2;
+
+//if (a3 > max) max = a3;
+//if (b3 > max) max = b3;
+//if (c3 > max) max = c3;
+
 //int max1 = Max(a1, b1, c1);
 //int max2 = Max(a2, b2, c2);
 //int max3 = Max(a3, b3, c3);
