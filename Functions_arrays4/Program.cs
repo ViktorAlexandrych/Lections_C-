0a45e65cbbdd609af85c0fa6a8ecd8582efe1a7e
Alexandrych <vyrupaev.00@gmail.com>
2023-07-18 15:55:24 +1000

    Этот коммит просто сохраняет

---
+++ Functions_arrays4/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+	int length = collection.Length;
+	int index = 0;
+	while (index < length)
+	{
+		collection[index] = new Random().Next(1, 10);
+		index++;
+	}
+}
+
+void PrintArray(int[] col)
+{
+	int count = col.Length;
+	int position = 0;
+	while (position < count)
+	{
+		System.Console.WriteLine(col[position]);
+		position++;
+	}
+}
+
+int IndexOf(int[] collection, int find)
+{
+	int count = collection.Length;
+	int index = 0;
+	int position = -1;
+	while (index < count)
+	{
+		if (collection[index] == find)
+		{
+			position = index;
+			break;
+		}
+		index++;
+	}
+	return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[7] = 8;
+array[2] = 8;
+
+PrintArray(array);
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 564);
+System.Console.WriteLine(pos);
+//Первый метод заполняет массив, а второй распечатывает его
