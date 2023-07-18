c10c77c752309c90ce87242e0f7ab40f37bff3e2
Alexandrych <vyrupaev.00@gmail.com>
2023-07-18 18:12:02 +1000

    Этот коммит сохраняет результаты изменений

---
+++ Methods3/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] arr = { 1, 23, 45, 5, 6, 7, 4, 32, 12, 43, 2, 1, 2 };
+
+void PrintArray(int[] array)
+{
+	int count = array.Length;
+	for (int i = 0; i < count; i++)
+	{
+		Console.Write($"{array[i]} ");
+	}
+	Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+	for (int i = 0; i < array.Length - 1; i++)
+	{
+		int minPosition = i;
+
+		for (int j = i + 1; j < array.Length; j++)
+		{
+			if (array[j] > array[minPosition]) minPosition = j;
+		}
+
+		int temporary = array[i];
+		array[i] = array[minPosition];
+		array[minPosition] = temporary;
+	}
+}
+
+PrintArray(arr);
+
+SelectionSort(arr);
+
+PrintArray(arr);
