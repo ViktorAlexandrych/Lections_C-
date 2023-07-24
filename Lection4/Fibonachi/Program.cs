c15a2b844cd7bb94923e60c79eca3a1f3c62c4ab
Alexandrych <vyrupaev.00@gmail.com>
2023-07-24 22:32:17 +1000

    This commit save lection 4

---
+++ Lection4/Fibonachi/Program.cs
@@ -0,0 +1,10 @@
+﻿
+double f(double n)
+{
+	if (n == 1 || n == 2) return 1;
+	else return f(n - 1) + f(n - 2);
+}
+for (double i = 1; i < 50; i++)
+{
+	System.Console.WriteLine($"f({i}) = {f(i)}");
+}
